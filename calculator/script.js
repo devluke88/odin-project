03e68ae58cc1719f2510f293adf7cf3169058544
lukasz.nowakiewicz <lukasz.nowakiewicz@gmail.com>
2022-04-25 21:10:12 +0100

    Add boiler plate for calculator

---
+++ calculator/script.js
@@ -0,0 +1,15 @@
+function add(n, m) {
+	return n + m;
+};
+
+function subtract (n, m) {
+	return n - m;
+};
+
+function multiply (n, m) {
+  return n * m;
+};
+
+function devide(n, m) {
+    return n / m;
+};
