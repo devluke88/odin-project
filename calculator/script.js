07f19024e2b1d59e9d21a0c2e8b356a822fb5dd4
lukasz.nowakiewicz <lukasz.nowakiewicz@gmail.com>
2022-04-25 22:23:09 +0100

    Update gui

--- calculator/script.js
+++ calculator/script.js
@@ -13,3 +13,28 @@ function multiply (n, m) {
 function devide(n, m) {
     return n / m;
 };
+
+function operate(operator, n, m) {
+    if (operator === "+") {
+        return add(n, m);
+    }
+    else if (operator === "-") {
+        return subtract(n, m);
+    }
+    else if (operator === "*") {
+        return multiply(n, m);
+    }
+    else if (operator === "/") {
+        return devide(n, m);
+    }
+
+};
+
+
+// TODO-2: function operate - take operator and call the right function
+
+// TODO-3: buttons for each digit, each functions and equals, clear button
+
+// TODO-4: function that populates the display when you click the number buttons, store display value in a var
+
+// TODO-5: 
