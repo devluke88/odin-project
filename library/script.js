f4b5f5d13670ed50587285e1fcd11adf1a7cb544
lukasz.nowakiewicz <lukasz.nowakiewicz@gmail.com>
2022-05-18 21:45:51 +0100

    Finish project library

--- library/script.js
+++ library/script.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 
+// Book constructor
 function Book(name, author, pages, read) {
   this.name = name,
   this.author = author,
@@ -7,37 +8,120 @@ function Book(name, author, pages, read) {
   this.read = read
 }
 
-function addBookToLibrary() {
-  let book = prompt("What is your favourite book?");
+// Add Book to Library
+function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-// TODO-3: Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
+// Reset Form once submitted
+function resetForm() {
+    modalTitle.value = "";
+    modalAuthor.value = "";
+    modalPages.value = "";
+    modalCheckbox.checked = false;
+}
+
+// Function to display all the Books from the Library
 function displayBook() {
+    cardsDiv.innerHTML = ""
     for(let lib in myLibrary) {
-        console.log(lib);
+        let readButton = ""
+        if (myLibrary[lib].read == "Read") {
+            readButton = `<div class="btn read" data-index-number="${lib}">${myLibrary[lib].read}</div>`;
+        }
+        else {
+            readButton = `<div class="btn not-read" data-index-number="${lib}">${myLibrary[lib].read}</div>`
+        }
+        let text = `<div class="card" data-card-index-number="${lib}"><div class="title">${myLibrary[lib].name}</div><div class="author">${myLibrary[lib].author}</div><div class="pages">${myLibrary[lib].pages}</div>${readButton}<div class="btn remove-btn" data-index-number="${lib}">Remove</div></div>`;
+        cardsDiv.innerHTML += text;
     }
 }
 
+// Remove Book from Library and card
+function removeBook(e) {
+    let idxNo = e.target.dataset.indexNumber;
+    myLibrary.splice(idxNo,1);
+    displayBook();
+}
+
+// Change the read status
+function changeReadStatus(e) {
+    let idxNo = e.target.dataset.indexNumber;
+    if (myLibrary[idxNo]["read"] == "Read") {
+        myLibrary[idxNo]["read"] = "Not read yet";
+    }
+    else {
+        myLibrary[idxNo]["read"] = "Read";
+    }
+    displayBook();
+};
+
+// Open Form to Add the Book
 function openModal() {
     modal.style.display = "block";
 };
 
+// Close Form when adding the Book
 function closeModal() {
     modal.style.display = "none";
 };
 
-//TODO-4: Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
+// Submit Form with the Book
+function onSubmit(e) {
+    let title = modalTitle.value;
+    let author = modalAuthor.value;
+    let pages = modalPages.value;
+    let checkbox = "";
+    if (modalCheckbox.checked != false) {
+        checkbox = "Read";
+    } else {
+        checkbox = "Not read yet";
+    }
+    let book = new Book(title, author, pages, checkbox);
+    e.preventDefault();
+    addBookToLibrary(book);
+    resetForm();
+    closeModal();
+    displayBook(); 
+}
+
+// Start Project - get basic variables
 const modal = document.querySelector(".modal");
 const addNewBook = document.querySelector(".add-new-book");
 addNewBook.addEventListener("click", openModal)
 const closeBtn = document.querySelector(".close");
 closeBtn.addEventListener("click", closeModal);
-// const submitBtn = document.querySelector(".submit");
-// submitBtn.addEventListener("click", alert("Test"));
+const submitBtn = document.querySelector(".submit");
+submitBtn.addEventListener("click", onSubmit);
 
+// Form part
+let modalTitle = document.querySelector(".modal-title");
+let modalAuthor = document.querySelector(".modal-author");
+let modalPages = document.querySelector(".modal-pages");
+let modalCheckbox = document.querySelector(".modal-checkbox");
+
+// Cards part
+let cardsDiv = document.querySelector(".cards"); 
+
+// Remove Button
+let removeBtns = document.querySelectorAll('.remove-btn');
+removeBtns.forEach(removeBtn => {
+    removeBtn.addEventListener("click", removeBook);
+});
+
+// Listen for clicks on remove or book read status button
+document.addEventListener('click',function(e){
+    if (e.target && e.target.classList[1] == 'remove-btn'){
+        removeBook(e);
+     }
+    else if (e.target && e.target.classList[1] == 'read' || e.target && e.target.classList[1] == 'not-read') {
+        changeReadStatus(e);
+    }
+ });
+
+// Close Modal when clicked on the background
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
-  }
+}
