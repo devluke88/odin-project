c4e6c3827b3d4eb963a52df8f383d237c852a1d2
lukasz.nowakiewicz <lukasz.nowakiewicz@gmail.com>
2022-06-08 23:11:15 +0100

    Finish project

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -68,12 +68,44 @@ const cardImagesList = [
     }
 ]
 
+// Function to display given selected section
+function getSection(e) {
+    e.preventDefault();
+    const clickedBtn = e.target.textContent.toLowerCase()
+    if (clickedBtn !== 'home') {
+        const element = document.getElementById(clickedBtn);
+        for (let i = 0; i < componentsList.length; i++) {
+            componentsList[i].style.display = 'none';
+        }
+        element.style.display = 'flex';
+        element.style.flex = '1 1 auto';
+        element.style.justifyContent = 'center';
+    }
+    else {
+        window.location.reload();
+    }
+};
+
+// Function to submit form
+function submitForm(e) {
+    e.preventDefault();
+    alert('Email sent!')
+}
+
 // Grab content
 const content = document.getElementById('content');
+content.style.display = 'flex';
+content.style.flexDirection = 'column';
+content.style.flex = '1 1 auto';
 
 // Navbar Section
 const navbar = navModule('Luca', 'Home', 'Menu', 'About', 'Contact');
 content.append(navbar);
+// Navbar element event listener
+const lis = document.querySelectorAll('li');
+lis.forEach(li =>{
+    li.addEventListener('click', getSection)
+});
 
 // Hero Section
 const heroHeaderText = 'Best Pizza!';
@@ -81,30 +113,33 @@ const heroSecText = 'We serve the best Neapolitan pizza accross the whole UK. Al
 const buttonText = 'Try Now';
 const hero = heroModule(heroHeaderText, heroSecText, buttonText, MainImage);
 content.append(hero);
+hero.style.display = 'flex';
+hero.style.flex = '1 1 auto';
 
 // Menu Section
 const menu = menuModule(cardImagesList);
 content.append(menu);
+menu.style.display = 'none';
 
 // About Section
 const aboutHeaderText = 'About us';
 const aboutSecText = 'We are small Italian family, owning ten restaurants accross the UK. We serve our community by delivering the best food in the World! We started this business to share the love and spread the kindnest by serving other people. We welcome everyone in our place. Do not hesitate to visit us!';
 const about = aboutModule(AboutImage, aboutHeaderText, aboutSecText);
 content.append(about);
+about.style.display = 'none';
 
 // Contact Section
-function submitForm(e) {
-    e.preventDefault();
-    alert('Email sent!')
-}
-
 const contact = contactModule('Contact us');
 content.append(contact);
 const form = document.querySelector('form');
 form.addEventListener('submit', submitForm);
+contact.style.display = 'none';
 
 // Footer Section
 const footer = document.createElement('div');
 footer.className = 'footer';
 footer.textContent = 'Copyright © Lukasz';
 content.append(footer);
+
+// Array with main components
+const componentsList = [hero, about, menu, contact]
