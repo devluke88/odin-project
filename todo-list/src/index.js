2213451c8400acff02c2cef94344ef8113f89545
lukasz.nowakiewicz <lukasz.nowakiewicz@gmail.com>
2022-06-12 15:34:06 +0100

    Add more files and start with basic ui

---
+++ todo-list/src/index.js
@@ -0,0 +1,22 @@
+import './style.css';
+
+let todoList = [];
+
+// TODO-3:
+// Your todo list should have projects or separate lists of todos. When a user first opens the app, there should be some sort of ‘default’ project to which all of their todos are put. Users should be able to create new projects and choose which project their todos go into.
+
+// TODO-4:
+// You should separate your application logic (i.e. creating new todos, setting todos as complete, changing todo priority etc.) from the DOM-related stuff, so keep all of those things in separate modules.
+
+// TODO-5:
+// The look of the User Interface is up to you, but it should be able to do the following:
+// a. view all projects
+// b. view all todos in each project (probably just the title and duedate.. perhaps changing color for different priorities)
+// c. expand a single todo to see/edit its details
+// d. delete a todo
+
+// TODO-6
+// For inspiration, check out the following great todo apps. (look at screenshots, watch their introduction videos etc.)
+// Todoist
+// Things
+// any.do
